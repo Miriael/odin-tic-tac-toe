63299fb144d3a41c28f2140cca4ff5d9d8858135
Miriael <ravafiel@protonmail.com>
2022-07-28 17:26:47 +0200

    Partially implement the main game flow

--- script.js
+++ script.js
@@ -8,25 +8,77 @@ const game = (() => {
         if (item.innerHTML == '') {
         gameBoard.addToBoard(currentPlayer, item.dataset.x, item.dataset.y);
         displayController.refresh();
+        switchCurrentPlayer();
+        evaluateBoard();
         } else {
-          alert('Invalid placement')
-        }
+          alert('Invalid placement');
+        };
       });
     });
   };
-  return {buttonSetUp, getCurrentPlayer, switchCurrentPlayer}
+  const evaluateBoard = () => {
+    let winner = ''
+    let rowpieces = []
+    let columnpieces = []
+    let nothing = new Set(' ')
+    for (let rowevalx = 0; rowevalx < 3; rowevalx++) {
+      rowpieces = [];
+      for (let rowevaly = 0; rowevaly < 3; rowevaly++) {
+        rowpieces.push(gameBoard.getBoard()[rowevalx][rowevaly])
+        };
+      if (new Set(rowpieces).size === 1 && new Set(rowpieces) !== nothing) {
+        console.log(Array.from(new Set(rowpieces)) != Array.from(nothing))
+        console.log(Array.from(new Set(rowpieces)))
+        console.log(Array.from(nothing))
+        winner = rowpieces[0];
+        endGame();
+      };
+    };
+    for (let vertevaly = 0; vertevaly < 3; vertevaly++) {
+      columnpieces = [];
+      for (let vertevalx = 0; vertevalx < 3; vertevalx++) {
+        columnpieces.push(gameBoard.getBoard()[vertevalx][vertevaly])
+        };
+      if (new Set(columnpieces).size === 1 && new Set(columnpieces) != new Set(' ')) {
+      winner = columnpieces[0];
+      //endGame();
+      };
+    };
+    if (new Set([gameBoard.getBoard()[0][0], gameBoard.getBoard()[1][1], gameBoard.getBoard()[2][2]]).size === 1 
+        && new Set([gameBoard.getBoard()[0][0], gameBoard.getBoard()[1][1], gameBoard.getBoard()[2][2]]) != new Set(' ')) {
+      winner = gameBoard.getBoard()[0][0];
+      //endGame();
+    };
+    if (new Set([gameBoard.getBoard()[0][2], gameBoard.getBoard()[1][1], gameBoard.getBoard()[2][0]]).size === 1
+        && new Set([gameBoard.getBoard()[0][2], gameBoard.getBoard()[1][1], gameBoard.getBoard()[2][0]]) != new Set(' '))
+      winner = gameBoard.getBoard()[0][2];
+      //endGame();
+  };
+  const startGame = () => {
+    playerOne = PlayerFactory('Player 1', 'X');
+    playerTwo = PlayerFactory('Player 2', 'O');
+    buttonSetUp();
+  };
+  const endGame = (winner) => {
+    alert(`Player ${winner} is victorious!`)
+  };
+  const restartGame = () => {
+    gameBoard.clearBoard();
+    switchCurrentPlayer();
+  };
+  return {buttonSetUp, getCurrentPlayer, evaluateBoard, startGame};
 })();
 
 const gameBoard = (() => {
-  let board = [['', '', ''],
-               ['', '', ''],
-               ['', '', '']];
+  let board = [[' ', ' ', ' '],
+               [' ', ' ', ' '],
+               [' ', ' ', ' ']];
   const getBoard = () => {return board};
   const addToBoard = (piece, x, y) => board[x][y] = piece; 
   const clearBoard = () => {for (let x = 0; x < board.length; x++) {
     for (let y = 0; y < board[x].length; y++) {
       console.log(board[x][y]);
-      board[x][y] = '';
+      board[x][y] = ' ';
     };
   }};
   return {addToBoard, clearBoard, getBoard};
@@ -59,3 +111,5 @@ const displayController = (() => {
   };
   return {refresh};
 })();
+
+game.startGame();
