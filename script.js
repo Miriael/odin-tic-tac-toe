837741cb0a2b378f7a9d02064134d90ed84fa749
Miriael <ravafiel@protonmail.com>
2022-07-28 15:34:03 +0200

    Add a way to interact with the board

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const game = (() => {
+  let currentPlayer = 'X';
+  const switchCurrentPlayer = () => currentPlayer == 'X' ? currentPlayer = 'O' : currentPlayer = 'X';
+  const getCurrentPlayer = () => {return currentPlayer};
+  const buttonSetUp = () => {
+    document.querySelectorAll('.board__cell').forEach((item) => {
+      item.addEventListener('click', () => {
+        if (item.innerHTML == '') {
+        gameBoard.addToBoard(currentPlayer, item.dataset.x, item.dataset.y);
+        displayController.refresh();
+        } else {
+          alert('Invalid placement')
+        }
+      });
+    });
+  };
+  return {buttonSetUp, getCurrentPlayer, switchCurrentPlayer}
+})();
+
 const gameBoard = (() => {
   let board = [['', '', ''],
                ['', '', ''],
@@ -7,18 +26,19 @@ const gameBoard = (() => {
   const clearBoard = () => {for (let x = 0; x < board.length; x++) {
     for (let y = 0; y < board[x].length; y++) {
       console.log(board[x][y]);
-      board[x][y] = 'ass';
+      board[x][y] = '';
     };
   }};
   return {addToBoard, clearBoard, getBoard};
 })();
 
-const PlayerFactory = (name) => {
+const PlayerFactory = (name, playerpiece) => {
   let score = 0;
   const getName = () => {return name};
   const getScore = () => {return score};
   const setScore= (val) => score = val;
-  return {getScore, setScore, getName};
+  const getPlayerPiece = () => {return playerpiece};
+  return {getScore, setScore, getName, getPlayerPiece};
 }; 
 
 const displayController = (() => {
